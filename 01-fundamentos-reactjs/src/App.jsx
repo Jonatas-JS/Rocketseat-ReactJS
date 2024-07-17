db661da1e07da7deeebdd2a3fee2674c66f3395f
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2024-07-17 12:50:31 -0300

    Starting Header creation

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,22 @@
+import './styles.css';
+import { Header } from './components/Header';
 import { Post } from './Post';
+
 
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Header />
+
+      <Post 
+        author="Jônatas Fernandes" 
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod saepe inventore numquam blanditiis voluptatem praesentium obcaecati illum quidem dolorem! Optio accusamus non alias assumenda possimus nihil earum veritatis voluptatum molestiae?" 
+      />
+
+      <Post 
+        author="Diego Fernandes"
+        content="Um novo post show de bola"
+      />
     </div>
     
   )
