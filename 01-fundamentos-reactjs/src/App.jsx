a26355776c658e81b9f3e1d1413b2ccf07322f8f
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2024-07-19 12:49:25 -0300

    Sidebar construction

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,6 +1,9 @@
 import './global.css';
+import styles from './App.module.css';
+
 import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
 
 
 export function App() {
@@ -8,16 +11,21 @@ export function App() {
     <div>
       <Header />
 
-      <Post 
-        author="Jônatas Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod saepe inventore numquam blanditiis voluptatem praesentium obcaecati illum quidem dolorem! Optio accusamus non alias assumenda possimus nihil earum veritatis voluptatum molestiae?" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Jônatas Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod saepe inventore numquam blanditiis voluptatem praesentium obcaecati illum quidem dolorem! Optio accusamus non alias assumenda possimus nihil earum veritatis voluptatum molestiae?"
+          />
 
-      <Post 
-        author="Diego Fernandes"
-        content="Um novo post show de bola"
-      />
+          <Post
+            author="Diego Fernandes"
+            content="Um novo post show de bola"
+          />
+        </main>
+      </div>
     </div>
-    
+
   )
 }
